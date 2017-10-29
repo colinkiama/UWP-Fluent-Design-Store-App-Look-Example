28176a8d3aad9a1fdcc0f707a7cbb294abbbcdae
Colin Kiama <colinkiama@hotmail.co.uk>
2017-10-29 04:44:50 +0000

    done

--- wtf/MainPage.xaml.cs
+++ wtf/MainPage.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Uwp.UI.Animations;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +28,15 @@ namespace wtf
         public MainPage()
         {
             this.InitializeComponent();
+            //Extending view into title bar to replicate store app
+            var appView = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView();
+            appView.TitleBar.BackgroundColor = Colors.Transparent;
+            appView.TitleBar.ButtonBackgroundColor = Colors.Transparent;
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+
+            //There are other ways of adding blur using the Community Toolkit. This is just for the solution.
+            header.Blur(30, 0, 0).Start();
         }
+
     }
 }
